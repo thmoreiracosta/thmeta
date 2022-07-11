df516f8e13df2557076a1c6e05d749c26b4b6855
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-07-11 10:49:25 -0300

    Iniciação projeto React/Spring - Aula 01

---
+++ backend/src/main/java/com/devsuperior/thmeta/ThmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.thmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThmetaApplication.class, args);
+	}
+
+}
