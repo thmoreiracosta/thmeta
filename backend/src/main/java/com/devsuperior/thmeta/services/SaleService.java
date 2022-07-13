284df8498dd53db2ba0791b369283ef97a7633dd
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-07-13 12:26:07 -0300

    DateSelect

--- backend/src/main/java/com/devsuperior/thmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/thmeta/services/SaleService.java
@@ -1,9 +1,15 @@
 package com.devsuperior.thmeta.services;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import com.devsuperior.thmeta.entities.Sale;
 import com.devsuperior.thmeta.repositories.SaleRepository;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 
 @Service
@@ -12,8 +18,14 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale>findSales() {
-		return repository.findAll();
+	public Page<Sale>findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 
 }
