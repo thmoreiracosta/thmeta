e610e2fc02b3b91352b8f433610024db739145c1
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-07-15 15:27:12 -0300

    variaveis de ambiente env

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,4 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
-export const VITE_BACKEND_URL = "https://thmeta.herokuapp.com";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? import.meta.env.VITE_LOCAL_URL;
 
 
