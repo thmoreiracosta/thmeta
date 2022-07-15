f1976b344a7a0148c495f69dec9de8929a0d3b27
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-07-15 19:13:59 -0300

    heroku insiste

--- backend/src/main/java/com/devsuperior/thmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/thmeta/services/SmsService.java
@@ -1,6 +1,6 @@
 package com.devsuperior.thmeta.services;
 
-import java.text.DecimalFormat;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,7 +37,7 @@ public class SmsService {
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
 		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date
-				+ " com um total de R$ " + String.format("%.0f", sale.getAmount());
+				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
