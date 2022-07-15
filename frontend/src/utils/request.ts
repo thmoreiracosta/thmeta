12e81b9f704c031e35102f479f73560a9e48fe26
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-07-15 14:02:33 -0300

    Toast teste rafael_1

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,8 +1,4 @@
 export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
 export const VITE_BACKEND_URL = "https://dsmeta-rafael.herokuapp.com";
-dfgsdfgsdf
-sdfgsdfg
-sdfgsdfgs
-sdfgsdfg
-sdfgsdfg
+
 
